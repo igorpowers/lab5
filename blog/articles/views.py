49ba4de048a7cee7c815ef55da01d9b7e281f811
igorpowers <powerfed@mail.ru>
2022-11-21 20:06:58 +0300

    lab5

---
+++ blog/articles/views.py
@@ -0,0 +1,36 @@
+from .models import Article
+from django.http import Http404
+from django.shortcuts import render,redirect
+
+def archive(request): 
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
+
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, 'article.html', {"post": post})
+    except Article.DoesNotExist:
+            raise Http404
+
+def create_post(request):
+    if True:# not request.user.is_anonymous():
+        if request.method == "POST":
+            form = {
+                'text': request.POST["text"], 'title': request.POST["title"]
+            }
+            if form["text"] and form["title"]:
+                try:
+                    Article.objects.get(title=form["title"])
+                    form['errors'] = u"Статья с таким названием уже существует!"
+                    return render(request, 'create_post.html', {'form': form});
+                except Article.DoesNotExist:
+                    article = Article.objects.create(text=form["text"], title=form["title"], author=request.user)
+                    return redirect('get_article', article_id=article.id)
+            else:
+                form['errors'] = u"Не все поля заполнены"
+                return render(request, 'create_post.html', {'form': form})
+        else:
+            return render(request, 'create_post.html', {})
+    # else:
+        # raise Http404
